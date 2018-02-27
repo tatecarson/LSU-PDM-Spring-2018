1b612926567370f252242305a2cfb6421f64e822
tatecarson <tatecarson@gmail.com>
2018-02-27 12:28:55 -0600

    added two envelopes

--- Week_2_T/1_Envelopes_FINISHED/sketch.js
+++ Week_2_T/1_Envelopes_FINISHED/sketch.js
@@ -1,6 +1,7 @@
 var osc,
 	ampEnv,
 	gainNode,
+	freqEnv,
 	modEnv;
 
 function setup() {
@@ -16,8 +17,29 @@ function setup() {
 		"release": 0.8,
 	});
 
+	freqEnv = new Tone.ScaledEnvelope({
+		"attack": 0.5,
+		"decay": 0.2,
+		"sustain": 1,
+		"release": 0.8,
+		"min": 200,
+		"max": 1000
+	});
+
+	modEnv = new Tone.ScaledEnvelope({
+		"attack": 0.5,
+		"decay": 0.2,
+		"sustain": 1,
+		"release": 0.8,
+		"min": 1,
+		"max": 100
+	});
+
+
 	//assign the envelope to the gain
 	ampEnv.connect(gainNode.gain);
+	freqEnv.connect(osc.frequency);
+	modEnv.connect(osc.modulationFrequency)
 
 	//now connect osc to gain node 
 	osc.connect(gainNode);
@@ -29,11 +51,16 @@ function keyPressed() {
 	if (keyCode == 49) {
 		osc.frequency.value = 300;
 		ampEnv.triggerAttackRelease(2);
+		freqEnv.triggerAttackRelease(2);
+		modEnv.triggerAttackRelease(2);
 		console.log("1");
 	} else if (keyCode == 50) {
 		console.log("2");
 		osc.frequency.value = 400;
+		freqEnv.triggerAttackRelease(2);
 		ampEnv.triggerAttackRelease(2);
+		modEnv.triggerAttackRelease(2);
+
 	} else if (keyCode == ENTER) {
 		ampEnv.attack = random(0.1, 1);
 		ampEnv.decay = random(0.2, 0.5);
