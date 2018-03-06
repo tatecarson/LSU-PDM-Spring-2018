8f6315b087723130e5a9299132ca03c7d9c3d265
tatecarson <tatecarson@gmail.com>
2018-03-06 12:46:59 -0600

    added start rhythm

--- Week_3_T/3_Rhythm_START/sketch.js
+++ Week_3_T/3_Rhythm_START/sketch.js
@@ -19,31 +19,19 @@ function setup() {
 	oscilloscope.connect(Tone.Master);
 
 
-	kicks = new Tone.Players({
-		'k1': 'dilla/ASD Dilla Kicks/ASD Dilla Kick 01.wav',
-		'k2': 'dilla/ASD Dilla Kicks/ASD Dilla Kick 02.wav'
-	}).toMaster();
-
-	hats = new Tone.Players({
-		'h1': 'dilla/ASD Dilla Hats/ASD Dilla Hat 01.wav'
-	}).toMaster();
-
-	snares = new Tone.Players({
-		's1': 'dilla/ASD Dilla Snares/ASD Dilla Snare 01.wav'
-	}).toMaster();
 
 	var fours = beet.pattern(4);
 	var threes = beet.pattern(2);
 	var seven = beet.pattern(3);
 
 	var layer = beet.layer(fours, (time) => {
-		kicks.get('k2').start();
+
 	});
 	var layer2 = beet.layer(threes, (time) => {
-		hats.get('h1').start();
+
 	});
 	var layer3 = beet.layer(seven, (time) => {
-		snares.get('s1').start();
+
 	})
 
 	beet.add(layer, layer2, layer3);
