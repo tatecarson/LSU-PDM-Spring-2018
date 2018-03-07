6a2210e13e51e1a0e8fb76e7165869b8744e16f6
tatecarson <tatecarson@gmail.com>
2018-03-06 23:42:43 -0600

    updating polyrhythms example

--- Week_3_T/1_FiltersExample_START/sketch.js
+++ Week_3_T/1_FiltersExample_START/sketch.js
@@ -1,3 +1,5 @@
+var polySynth, lfo;
+
 function setup() {
 	//Visualization 
 	//setup audio context 
@@ -10,9 +12,44 @@ function setup() {
 	var oscilloscope = new Nexus.Oscilloscope('#osc')
 	oscilloscope.connect(Tone.Master);
 
+	polySynth = new Tone.PolySynth(8, Tone.Synth);
+	filter = new Tone.Filter(1000, 'lowpass').toMaster()
 
+	lfo = new Tone.LFO({
+		type: 'sine',
+		min: 100,
+		max: 1000,
+		frequency: '8n'
+	}).start();
+
+	var autoFilter = new Tone.AutoFilter({
+		frequency: 1,
+		type: 'sine',
+		depth: 2,
+		baseFrequency: 200,
+		octaves: 2.6,
+		filter: {
+			type: 'lowpass',
+			rolloff: -12,
+			Q: 1
+		}
+	}).toMaster().start()
+
+	// polySynth.connect(filter);
+	polySynth.connect(autoFilter);
+	lfo.connect(filter.frequency);
+
+	var loop = new Tone.Loop((time) => {
+		polySynth.triggerAttackRelease('c4', time);
+		autoFilter.frequency.value = random(1, 30);
+	}, '1n').start();
+
+	Tone.Transport.bpm = 200;
+	// Tone.Transport.start();
 }
 
 function keyPressed() {
-
+	if (keyCode == 49) {
+		polySynth.triggerAttackRelease('c3', 2);
+	}
 }
--- Week_3_T/2_Rhythm_START/sketch.js
+++ Week_3_T/2_Rhythm_START/sketch.js
@@ -2,7 +2,10 @@
 var polySynth, monoSynth;
 
 function setup() {
-
+	var beet = new Beet({
+		context: Tone.context,
+		tempo: 100
+	});
 
 	//Visualization 
 	//setup audio context 
@@ -15,5 +18,31 @@ function setup() {
 	var oscilloscope = new Nexus.Oscilloscope('#osc')
 	oscilloscope.connect(Tone.Master);
 
+	polySynth = new Tone.PolySynth(8, Tone.Synth).toMaster()
 
+	var four = beet.pattern(4);
+	var three = beet.pattern(3);
+	var seven = beet.pattern(7);
+
+	var scale = ['c4', 'd4', 'e4', 'f#4'];
+
+	var i = 0;
+	var layer = beet.layer(four, (time) => {
+		polySynth.triggerAttackRelease(scale[i], '16n', time);
+		console.log(i);
+		i = i % scale.length - 1;
+		i++;
+	});
+
+	var layer2 = beet.layer(three, (time) => {
+		polySynth.triggerAttackRelease('G4', '16n', time);
+	});
+
+	var layer3 = beet.layer(seven, (time) => {
+		polySynth.triggerAttackRelease('B4', '8n', time);
+	});
+
+	beet.add(layer, layer2, layer3);
+
+	// beet.start();
 }
