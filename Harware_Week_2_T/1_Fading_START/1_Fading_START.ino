77cfb9019d74cb7c4281b9d22c77cb904be94ae1
tatecarson <tatecarson@gmail.com>
2018-03-19 22:44:53 -0500

    removed start code comments

--- Harware_Week_2_T/1_Fading_START/1_Fading_START.ino
+++ Harware_Week_2_T/1_Fading_START/1_Fading_START.ino
@@ -1,21 +1,2 @@
-/*
-  Fading
-
-  This example shows how to fade an LED using the analogWrite() function.
-
-  The circuit:
-  - LED attached from digital pin 9 to ground.
-
-  created 1 Nov 2008
-  by David A. Mellis
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Fading
-*/
-
 //for intellisense in vscode
 #include <Arduino.h>
-
